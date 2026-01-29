ce04b6b2f7f2da6f74a7107ef9fc11fcae4f92ca
kartavyakrishnsrivastava-pixel <kartavyakrishnsrivastava@gmail.com>
2026-01-30 02:00:54 +0530

    input function

---
+++ inputandint.py
@@ -0,0 +1,13 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print("The first number is:", a)
+print("The second number is:", b)
+
+print("The sum is:", a + b) # this will concatenate the inputs as strings as input takes input as string by default
+# To perform addition, we need to convert the inputs to integers
+c= int(a)
+d= int(b)
+print(c)
+print(d)
+5
+print("The sum after converting to integers is:", c + d)
