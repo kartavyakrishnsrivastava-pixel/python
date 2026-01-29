9b41161de35f9d86e39f9df4a4612164c242a677
kartavyakrishnsrivastava-pixel <kartavyakrishnsrivastava@gmail.com>
2026-01-30 01:45:42 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,20 @@
+e = True or False
+# --- IGNORE ---
+print(e)
+#truth table for "or" operator same as or logic gate which i have studied in digital electronics
+
+print("truth table for \"or\" operator")
+print("-------------------------------------")
+print("true or true is:", True or True)
+print("true or false is:", True or False)
+print("false or true is:", False or True)   
+print("false or false is:", False or False)
+
+print("truth table for \"and\" operator")
+print("---------------------------------")
+# now for "and" operator same as and logic gate which i have studied in digital electronics
+print("true and true is:", True and True)
+print("true and false is:", True and False) 
+print("false and true is:", False and True)
+print("false and false is:", False and False)
+
