ae4b3ce34186b4364b5b69f34be3006f82f8da78
kartavyakrishnsrivastava-pixel <kartavyakrishnsrivastava@gmail.com>
2026-01-30 01:35:20 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,54 @@
+
+#problem 1 
+print('''
+      Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+How I
+       wonder what you are!
+''')
+
+
+# problem 2
+
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. When the blazing sun is gone, When he nothing shines upon, Then you show your little light, Twinkle, twinkle, all the night. Then the traveler in the dark Thanks you for your tiny spark, How could he see where to go, If you did not twinkle so? In the dark blue sky you keep, Often through my curtains peep For you never shut your eye, Till the sun is in the sky. As your bright and tiny spark Lights the traveler in the dark, Though I know not what you are, Twinkle, twinkle, little star. How I wonder what you are!")
+engine.runAndWait()
+
+
+a = 3 
+b = 5  #this is a integer
+print(a+b)
+ 
+
+c= "hello " #this is a string
+d= "world"
+print(c+d)
+print("The sum of", a, "and", b, "is", a + b)
+e = 2.5  #this is a float
+f = False  #this is a boolean
+g = None  #this is a NoneType
+print(g)
