0561be4935ce9ee78b1071df6b6275c8acb8fcbd
Kartavya Krishn Srivastava <kartavyakrishnsrivastava@gmail.com>
2026-02-02 15:42:10 +0530

    BASE IF ELSE

---
+++ hjjh.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your age: "))
+if a>=18:
+    print("You are eligible to vote.")
+    print("Please cast your vote wisely. SARKAR CHUNNA HAI BAGWAN NAHI BANNA.")
+else:
+    print("You are not eligible to vote.")
